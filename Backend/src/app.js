8b82fd2f4d109f029b2e0dc1ec5e99faee1410b6
Pilar <nadaalpilarr@gmail.com>
2026-04-22 19:22:18 -0300

    importe la carpeta auth.routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,23 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import  pool  from './db.js';
+import authRoutes from './routes/auth.routes.js';
 
 const app= express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/auth', authRoutes);
 
-//ruta de prueba
-app.get('/usuarios', async (req, res) =>{
-    try {
-        const result = await Pool.query('SELECT * FROM usuarios');
-        res.json(result.rows);
 
-    } catch (err){
-        console.error(error);
-        res.status(500).json({error: 'error en el servidor'})
-    }
-});
 
 app.listen(3000,()=>{
     console.log('servidor corriendo en el puerto 3000');
