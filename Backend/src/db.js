ee344290a8a61f7a14989c1637475bd34e189abd
Pilar <nadaalpilarr@gmail.com>
2026-04-22 19:19:03 -0300

    variable de entorno corregida con exito

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -8,13 +8,15 @@ const pool = new Pool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    database: process.env.DB_NAME,
     port: process.env.DB_PORT,
 });
 
 //conexion a la db
-pool.connect()
+pool.query('SELECT 1')
 .then(() => console.log('conectado a la db'))
-.catch((err=> console.error('error al conectarse a la db', err)));
+.catch((err => console.error('error al conectarse a la db', err)));
+
+console.log("DB:", process.env.DB_NAME);
 
 export default pool;
